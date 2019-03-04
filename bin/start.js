5d19d8276a9c1403b4c7679d5668016108ed302f
Tali Escherˆ <t.escher@holidaypirates.com>
2019-03-04 12:49:25 +0100

    feat: vgh commit and push

---
+++ bin/start.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const GHP = require("gh-pages");
+const SHELL = require("shelljs");
+
+exports.run = function(params) {
+  GHP.clean();
+  const targetDirectory = `${process.cwd()}/${params.dir}`;
+
+  const options = {
+    branch: params.branch || "gh-pages",
+    dest: ".",
+    dotfiles: params.dotfiles || false,
+    add: params.keepExistingFiles || false,
+    tag: params.tag && params.tag,
+    message: params.message,
+    user: { name: params.name, email: params.email } || undefined
+  };
+
+  (() => {
+    new Promise((resolve, reject) => {
+      if (params.skipBuild) {
+        console.log(`Ahoy => you decided to skip the building proccess.`);
+        return resolve();
+      }
+      SHELL.exec('npm run build', (code, out, err) => {
+        if (code) {
+          return reject(err);
+        }
+        console.log(`Ahoy => building proccess complete`);
+        return resolve();
+      })
+    }).catch((err) => {
+      console.log(
+        `Oh noes => Something went wrong during the building proccess. Here's the output: ${err}`
+      );
+    }).then(() =>  {
+      GHP.publish(targetDirectory, options)
+    }).then(() => {
+      console.log(`Ahoy => deploy proccess complete. Make sure you have GitHub Pages enabled in this project settings ok?`)
+    }).catch((err) => {
+      console.log(`Oh noes => Something went wrong during the building proccess. Here's the output: ${err}`)
+    })
+  })()
+};
--- bin/vgh.js
+++ bin/vgh.js
@@ -1,4 +1,61 @@
-#! /usr/bin/env node
-var shell = require("shelljs");
+#!/usr/bin/env node
 
-shell.exec("vgh-commit && vgh-push");
+"use strict";
+
+const start = require("./start");
+const PKG = require("../package.json");
+const options = require("commander");
+
+options
+  .description(PKG.description)
+  .version(PKG.version, "-v, -V, --version")
+  .option(
+    "-d, --dir <dir>",
+    "Directory of the content to be published. /dist is the default",
+    "dist"
+  )
+  .option(
+    "-p, --public-path <path>",
+    "by default we assume you want your app to be deployed at the root of your domain. If it's not the case, set this variable as the name of the path."
+  )
+  .option(
+    "-a, --add-dotfiles",
+    "by default git ignores files starting with a '.'; To add them set this flag to true"
+  )
+  .option(
+    "-b, --branch <branch>",
+    "The git branch target for your GitHub Pages",
+    "gh-pages"
+  )
+  .option(
+    "-e, --email <email>",
+    "The git user-email which is associated with this commit"
+  )
+  .option(
+    "-k, --keep-existing-files",
+    "by default git cleans existing files before adding the new ones. Setting this to true leaves the existing ones untouched"
+  )
+  .option(
+    "-m, --message <message>",
+    "The commit message",
+    "Updates for GitHub Pages"
+  )
+  .option(
+    "-n, --name <name>",
+    "The git user-name which is associated with this commit."
+  )
+  .option(
+    "-s, --skip-build",
+    "skips building process. If not set the project will be built"
+  )
+  .option(
+    "-t, --tag <tag>",
+    "Create a tag after committing changes on the target branch. By default, no tag is created."
+  )
+  .parse(process.argv);
+
+try {
+  start.run(options);
+} catch (err) {
+  process.exit(1);
+}
